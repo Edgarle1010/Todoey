9b6f2449156391ae4ec282a7204f28bfd9cd64db
Edgar López Enríquez <edgar_fl1012@hotmail.com>
2018-10-30 21:04:38 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Edgar López Enríquez on 10/30/18.
+//  Copyright © 2018 Edgar López Enríquez. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
